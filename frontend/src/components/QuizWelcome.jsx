35b29a7b9997a782cc805c4d90ae7347e88f9714
LGoGoGo <19931299891@163.com>
2023-10-02 21:14:33 +1300

    Beautify quiz component

--- frontend/src/components/QuizWelcome.jsx
+++ frontend/src/components/QuizWelcome.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import { useNavigator } from '../hooks/useNavigator'
+import {Button} from 'antd-mobile'
+import './QuizWelcome.css';
 
 export default function QuizWelcome({ quizData, onAccept, onReject }) {
     const navigateWithState = useNavigator();
 
     return (
-        <div>
+        <div className="quiz-welcome-container">
             {quizData.isFlowHelper ?
-                <p>Looks like you haven't find any bird for a while, answering this quiz correctly will reward you <span onClick={() => navigateWithState(`/bird/${quizData.birdName}`, { replace: true })}
+                <p className="quiz-description">Looks like you haven't find any bird for a while, answering this quiz correctly will reward you <span onClick={() => navigateWithState(`/bird/${quizData.birdName}`, { replace: true })}
                     style={{ color: 'blue', textDecoration: 'underline', cursor: 'pointer' }}>
                     {quizData.birdName}</span> (rarity={quizData.birdRarity})!</p>
                 :
-                <p>Conguradulation! You just found <span onClick={() => navigateWithState(`/bird/${quizData.birdName}`, { replace: true })}
+                <p className="quiz-description">Conguradulation! You just found <span onClick={() => navigateWithState(`/bird/${quizData.birdName}`, { replace: true })}
                     style={{ color: 'blue', textDecoration: 'underline', cursor: 'pointer' }}>
                     {quizData.birdName}</span> (rarity={quizData.birdRarity})! You have got the reward for that, but answering this quiz will give you extra scores!</p>
             }
-            <button onClick={onAccept}>Start the Quiz</button>
-            <button onClick={onReject}>Not Interesting</button>
+            <Button className="quiz-button" color='primary' onClick={onAccept}>Start the Quiz</Button>
+            <Button className="quiz-button" color='primary' onClick={onReject}>Not Interested</Button>
         </div>
     )
 }
