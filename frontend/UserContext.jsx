1f6fa63bbe974cc29aa1aa1f99adec947510f54a
D-Z-2020 <zzha248@aucklanduni.ac.nz>
2023-10-20 20:07:10 +1300

    Add more comments

--- frontend/UserContext.jsx
+++ frontend/UserContext.jsx
@@ -1,5 +1,9 @@
 import { createContext } from 'react';
 
+// Creating a UserContext with default values. 
+// This context will provide a way to pass down 
+// user-related data and functions throughout the component tree 
+// without having to pass props manually at every level.
 const UserContext = createContext({
     username: null,
     setUsername: () => { },
